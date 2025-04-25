c3861ded95aa2cbe1f11d7ff9b4ce15cb49a604a
U027065 <jose.beneitez.ext@rsi.cajarural.com>
2025-04-26 01:55:24 +0200

    Agregado service con axios

---
+++ src/services/apiService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import * as constants from "@/js/constants";
+
+const httpClient = axios.create({
+  baseURL: constants.API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    [constants.CONTENT_TYPE]: constants.CONTENT_TYPE_APPLICATION_JSON,
+  },
+});
+
+export const apiService = {
+  get(url, config = {}) {
+    return httpClient.get(url, config);
+  },
+  post(url, data, config = {}) {
+    return httpClient.post(url, data, config);
+  },
+};
---
+++ src/services/productService.js
@@ -0,0 +1,13 @@
+import { apiService } from "@/apiService";
+
+export function getListProduct() {
+  return apiService.get("/api/product");
+}
+
+export function getProductDetail(productId) {
+  return apiService.get(`/api/product/${productId}`);
+}
+
+export function postProductToCart(cartData) {
+  return apiService.post("/api/cart", cartData);
+}
